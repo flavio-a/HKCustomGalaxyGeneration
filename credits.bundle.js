8f74330bfb0360b1be66a0ea9451e5ca896fd262
Flavio <11007745+flavio-a@users.noreply.github.com>
2026-06-02 06:48:57 +0000

    Updates

--- credits.bundle.js
+++ credits.bundle.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";document.getElementById("versionDisplay").textContent="Version 1.0.0"})();
+(()=>{"use strict";document.getElementById("versionDisplay").textContent="Version 1.1.4"})();
